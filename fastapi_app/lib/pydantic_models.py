ebac2d55528f9a4fb7503b59c2d04726822064a4
Eccllair <bright.stellar.accretion@gmail.com>
2024-05-28 16:15:36 +0700

    extended pydantic models

--- fastapi_app/lib/pydantic_models.py
+++ fastapi_app/lib/pydantic_models.py
@@ -1,10 +1,20 @@
 
+#sys imort
 import os
 from dotenv import load_dotenv
 
+#std import
 from datetime import datetime, timedelta
+from enum import Enum
 
+#web import
 from pydantic import BaseModel
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+#custom import
+from ..database import async_session_maker
+from ..models import User
 
 
 load_dotenv()
@@ -12,14 +22,23 @@ load_dotenv()
 JWT_ACCESS_LIFETIME = int(os.getenv('JWT_ACCESS_LIFETIME'))
 JWT_REFRESH_LIFETIME = int(os.getenv('JWT_REFRESH_LIFETIME'))
 
+class roles(Enum):
+    user = 0
+    admin = 1
+    superuser = 2
 
 class pd_signup_user(BaseModel):
     login: str
     mail: str
-    pwd_hash: str
+    password: str
+    
+class pd_user_role(BaseModel):
+    id: int
+    role: roles
 
 class pd_user(BaseModel):
-    login: str
+    id: int
+    login: str | None
     name: str | None
     surname: str | None
     patronymic: str | None
@@ -44,3 +63,6 @@ class pd_jwt(BaseModel):
         if expiration_date: new_expiration_date = expiration_date
         else: new_expiration_date = datetime.now() + timedelta(days=JWT_REFRESH_LIFETIME if is_refresh else JWT_ACCESS_LIFETIME)
         super().__init__(login=login, is_refresh=is_refresh, creation_date=str(new_creation_date), expiration_date=str(new_expiration_date))
+    
+    async def get_user(self, session: Session = async_session_maker()) -> User:
+        return await session.execute(select(User).where(User.login == self.login))
