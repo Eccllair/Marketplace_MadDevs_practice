3087027e1d1ae150b7b5b0f4d4036c5ae4a6991b
Eccllair <bright.stellar.accretion@gmail.com>
2024-05-30 16:27:41 +0700

    continued development of the shop router

--- fastapi_app/routers/shop_router.py
+++ fastapi_app/routers/shop_router.py
@@ -16,10 +16,10 @@ from sqlalchemy.exc import NoResultFound, IntegrityError
 from sqlalchemy.orm import Session
 from sqlalchemy.engine import Result
 
-from ..lib.pydantic_models import pd_shop, pd_shop_edit, pd_position
+from ..lib.pydantic_models import pd_shop, pd_shop_edit, pd_position, pd_position_edit
 from ..lib.secure import create_jwt, check_jwt, check_email, get_current_user, bcrypt_context
-from ..lib.exceptions import response_not_enough_rights
-from ..lib.responses import response_ok
+from ..lib.exceptions import NotFound, Forbidden
+from ..lib.responses import JResponse
 from ..models import User, Shop, ShopImage, ShopAndUser, Position
 from ..database import get_async_session
 
@@ -42,11 +42,7 @@ async def get_shops(cur_user: User = Depends(get_current_user), session: Session
             }
         )
     
-    return JSONResponse({
-        "status" : "ok",
-        "message" : "success.",
-        "body" : shops_response
-    })
+    return JResponse(body=shops_response)
 
 
 @shop_router.get("/{id}")
@@ -56,32 +52,23 @@ async def get_shop(id:int, cur_user: User = Depends(get_current_user), session: 
         shop: dict = shop_result.mappings().one()
     except NoResultFound as e:
         logging.error(f"404 GET shop error:\n{e._message()}")
-        return JSONResponse(
-            content={
-            "status" : "fail",
-            "message" : f"shop with id [{id}] does not exists"
-            },
-            status_code=404
-        )
+        return NotFound(message=f"shop with id [{id}] does not exists")
     
     shop_images_result: Result = session.execute(select(ShopImage.src).where(ShopImage.shop_id == shop.id))
     shop_images: list = shop_images_result.scalars().all()
     
-    return JSONResponse({
-        "status" : "ok",
-        "message" : "success.",
-        "body" : {
-            "shop" : shop,
-            "images" : shop_images
-        }
-    })
+    body = {
+        "shop" : shop,
+        "images" : shop_images
+    }
+    return JResponse(body=body)
 
 
 @shop_router.post("/")
 async def create_shop(shop: pd_shop, cur_user: User = Depends(get_current_user), session: Session = Depends(get_async_session)):
     await session.execute(insert(Shop).values(owner_id=cur_user.id, name=shop.name, description=shop.description, avatar_img=shop.avatar_img))
     await session.commit()
-    return response_ok
+    return JResponse()
 
 
 @shop_router.patch("/")
@@ -92,20 +79,8 @@ async def edit_shop(shop: pd_shop_edit, cur_user: User = Depends(get_current_use
         shop_db: Shop = shop_db_r.mappings().one()
     except NoResultFound as e:
         logging.error(f"PATCH shop error: {e._message()}")
-        return JSONResponse(
-            content={
-                "status" : "fail",
-                "message" : f"shop with id [{shop.id}] does not exists"
-            },
-            status_code=404
-        )
-    if shop_db.owner_id != cur_user.id: return JSONResponse(
-        content={
-            "status" : "fail",
-            "message" : "only shop owner can change shop"
-        },
-        status_code=403
-    )
+        return NotFound(message=f"shop with id [{shop.id}] does not exists")
+    if shop_db.owner_id != cur_user.id: return Forbidden(message="only shop owner can change shop")
     
     #редактирвние записи магазина
     values: dict = shop.model_dump(exclude_none=True, exclude={"id"})
@@ -115,11 +90,7 @@ async def edit_shop(shop: pd_shop_edit, cur_user: User = Depends(get_current_use
     #формирование ответа
     new_shop_r: Result = await session.execute(select(Shop.id, Shop.owner_id, Shop.name, Shop.description, Shop.avatar_img, Shop.is_confirmed).where(Shop.id == shop.id))
     new_shop: dict = new_shop_r.mappings().one()
-    return JSONResponse({
-        "status" : "ok",
-        "message" : "shop updated",
-        "body" : new_shop
-    })
+    return JResponse(message="shop updated", body=new_shop)
 
 
 @shop_router.delete("/")
@@ -130,20 +101,8 @@ async def delete_shop(shop_id: int, cur_user: User = Depends(get_current_user), 
         shop_db: Shop = shop_db_r.mappings().one()
     except NoResultFound as e:
         logging.error(f"DELETE shop error: {e._message()}")
-        return JSONResponse(
-            content={
-                "status" : "fail",
-                "message" : f"shop with id [{shop_id}] does not exists"
-            },
-            status_code=404
-        )
-    if shop_db.owner_id != cur_user.id: return JSONResponse(
-        content={
-            "status" : "fail",
-            "message" : "only shop owner can delete shop"
-        },
-        status_code=403
-    )
+        return NotFound(message=f"shop with id [{shop_id}] does not exists")
+    if shop_db.owner_id != cur_user.id: return Forbidden(message="only shop owner can delete shop")
     
     #удаление магазина
     await session.execute(delete(Shop).where(Shop.id == shop_id))
@@ -199,47 +158,105 @@ async def get_job_titles(id: int, cur_user: User = Depends(get_current_user), se
         )
     except NoResultFound as e:
         logging.error(f"GET position error: {e._message()}")
-        return JSONResponse(
-            content={
-                "status" : "fail",
-                "message" : f"position with id [{id}] does not exists"
-            },
-            status_code=404
-        )
+        return NotFound(message=f"position with id [{id}] does not exists")
     position: dict = position_r.mappings().one()
-    if position.creator_id != cur_user.id: return JSONResponse(
-        content={
-            "status" : "fail",
-            "message" : "you cannot get this position"
-        },
-        status_code=403
-    )
+    if position.creator_id != cur_user.id: return Forbidden(message="you cannot get this position")
     position.pop("creator_id")
-    return JSONResponse({
-        "status" : "ok",
-        "message" : "success.",
-        "body" : position
-    })
+    return JResponse(body=position)
 
 
 @shop_router.post("/positions")
-async def create_job_title(cur_user: User = Depends(get_current_user), session: Session = Depends(get_async_session)):
-    pass
+async def create_job_title(position: pd_position, cur_user: User = Depends(get_current_user), session: Session = Depends(get_async_session)):
+    values = position.model_dump(exclude_none=True)
+    await session.execute(
+        insert(Position)
+        .values(
+            creator_id = cur_user.id,
+            **values
+        )
+    )
+    await session.commit()
+    return JResponse()
 
 
 @shop_router.patch("/positions")
-async def edit_job_title(cur_user: User = Depends(get_current_user), session: Session = Depends(get_async_session)):
-    pass
+async def edit_job_title(position: pd_position_edit, cur_user: User = Depends(get_current_user), session: Session = Depends(get_async_session)):
+    values = position.model_dump(exclude_none=True, exclude={"id"})
+    try:
+        old_position_r: Result = await session.execute(select(Position).where(Position.id == position.id))
+        old_position: Position = old_position_r.scalar_one()
+    except NoResultFound as e:
+        logging.error(f"404 edit position error: {e._message()}")
+        return NotFound(message=f"position with id [{position.id}] does not exist")
+    if old_position.creator_id != cur_user.id: return Forbidden()
+    
+    
+    
+    await session.execute(update(Position).values(values).where(Position.id == position.id))
+    await session.commit()
+    position_r: Result = await session.execute(select(Position).where(Position.id == position.id))
+    new_position: list[dict] = [dict(pos) for pos in position_r.scalars().all()]
+    new_position.pop("creator_id")
+    return NotFound(message=f"position with id [{position.id}] does not exist")
 
 
 @shop_router.delete("/positions")
-async def delete_job_title(cur_user: User = Depends(get_current_user), session: Session = Depends(get_async_session)):
-    pass
+async def delete_job_title(position_id: int, cur_user: User = Depends(get_current_user), session: Session = Depends(get_async_session)):
+    try:
+        position_r: Result = await session.execute(select(Position).where(Position.id == position_id))
+    except NoResultFound as e:
+        logging.error(f"404 DELETE position not found: {e._message()}")
+        return NotFound(message=f"position with id [{position_id}] does not exist")
+    position: Position = position_r.scalar_one()
+    if position.creator_id != cur_user.id: return Forbidden(message="you can delete position another owner")
+    
+    await session.execute(delete(Position).where(Position.id == position_id))
+    await session.commit()
+    return JResponse()
 
 
 @shop_router.get("/staff")
 async def get_staff(cur_user: User = Depends(get_current_user), session: Session = Depends(get_async_session)):
-    pass
+    """returns all the user's staff"""
+    staff_r: Result = await session.execute(
+        select(
+            User.id, User.login, User.name, User.surname, User.avatar_img,
+            Position.id, Position.name,
+            Shop.id, Shop.name
+        )
+        .from_statement(ShopAndUser)
+        .join(User, User.id == ShopAndUser.user_id)
+        .join(Shop, Shop.id == ShopAndUser.shop_id)
+        .join(Position, Position.id == ShopAndUser.position_id)
+        .where(Shop.owner_id == cur_user.id)
+    )
+    staff: list[dict] = staff_r.mappings().all()
+    return JResponse(body=staff)
+
+@shop_router.get("/shop-staff")
+async def get_staff(shop_id:int, cur_user: User = Depends(get_current_user), session: Session = Depends(get_async_session)):
+    """returns all users of the selected shop"""
+    try:
+        shop_r: Result = await session.execute(select(Shop).where(Shop.id == shop_id))
+    except NoResultFound as e:
+        logging.error(f"404 GET shop-staff, shop not found: {e._message()}")
+        return NotFound(message=f"shop with id [{shop_id}] does not exist")
+    shop: Shop = shop_r.scalar_one()
+    if shop.owner_id != cur_user.id: return Forbidden(message="you can't get someone else's store")
+    
+    
+    staff_r: Result = await session.execute(
+        select(
+            User.id, User.login, User.name, User.surname, User.avatar_img,
+            Position.id, Position.name
+        )
+        .from_statement(ShopAndUser)
+        .join(User, User.id == ShopAndUser.user_id)
+        .join(Position, Position.id == ShopAndUser.position_id)
+        .where(ShopAndUser.shop_id == shop_id)
+    )
+    staff: list[dict] = staff_r.mappings().all()
+    return JResponse(body=staff)
 
 
 @shop_router.get("/staff/{id}")
