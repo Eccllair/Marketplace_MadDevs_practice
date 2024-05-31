6ff237a6d0b4023ed7c3946c0f49744bb88efeb7
Eccllair <bright.stellar.accretion@gmail.com>
2024-05-31 16:00:13 +0700

    connect shop router

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from .routers.user_router import user_router, auth_router
+from .routers.shop_router import shop_router
 
 load_dotenv()
 LOG_PATH = os.getenv('LOG_PATH')
@@ -15,13 +16,18 @@ tags_metadata = [
     },
     {
         "name": "users",
-        "description": "actions with the user.",
+        "description": "actions with user.",
         # "externalDocs": {
         #     "description": "Items external docs",
         #     "url": "https://fastapi.tiangolo.com/",
         # },
     },
+    {
+        "name": "shops",
+        "description": "actions with shop.",
+    },
 ]
+API_VERSION="/api/v1"
 
 app = FastAPI(openapi_tags=tags_metadata)
 
@@ -29,12 +35,18 @@ logging.basicConfig(filename=LOG_PATH, level=logging.INFO)
 
 app.include_router(
     router=auth_router,
-    prefix="/api/v1",
+    prefix=f"{API_VERSION}",
     tags=["auth"]
 )
 
 app.include_router(
     router=user_router,
-    prefix="/api/v1/users",
+    prefix=f"{API_VERSION}/users",
     tags=["users"]
+)
+
+app.include_router(
+    router=shop_router,
+    prefix=f"{API_VERSION}/shops",
+    tags=["shops"]
 )
